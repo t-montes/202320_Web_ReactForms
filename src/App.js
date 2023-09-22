c14943a96308fd9281675fff891d6980293d94d2
t-montes <t.montes@uniandes.edu.co>
2023-09-22 09:08:49 -0500

    Forms created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+const classes = [
+  {id:1, name:"ISIS3710"},
+  {id:2, name:"Programación con tecnologias web"}
+]
 
 function App() {
+  const [formValues, setFormValues] = useState({email:"", password:"", favClass:"1"})
+  const [validationStates, setValidationStates] = useState({emailState:true, passwordState:true})
+
+  const handleFormChange = ((e) => {
+    switch(e.target.name){
+      case "password":
+        setValidationStates({...validationStates, passwordState: validatePassword(e.target.value)});
+        break;
+      default:
+        break;
+    }
+    setFormValues({...formValues, [e.target.name]: e.target.value});
+  });
+
+  const clickSubmit = (() => {
+    const emailState = validateEmail(formValues.email);
+    const passwordState = validatePassword(formValues.password);
+    setValidationStates({
+      emailState, passwordState
+    });
+    if (emailState && passwordState)
+      alert(JSON.stringify(formValues));
+  });
+
+  const validateEmail = ((email) => {
+    const re = /\S+@\S+\.\S+/;
+    const isValid = re.test(email);
+    console.log("validateEmail", isValid);
+    return isValid;
+  });
+
+  const validatePassword = ((password) => {
+    const re = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{9,}$/;
+    const isValid = re.test(password);
+    console.log("validatePassword", isValid);
+    return isValid;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ejemplo de formularios!</h1>
+     
+      <Form>
+      <Form.Group className="mb-6" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" onChange={handleFormChange} name="email" className={
+          !validationStates.emailState && "invalid"}/>
+        { !validationStates.emailState && <Form.Text className="text-muted">The email is not valid</Form.Text>}
+      </Form.Group>
+ 
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" onChange={handleFormChange} name="password" className={
+          !validationStates.passwordState && "invalid"}/>
+        { !validationStates.passwordState && <Form.Text className="text-muted">Your password should be have numbers and letters and should be at least 9 char long</Form.Text>}
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Label>Favorite Class</Form.Label>
+        <Form.Select onChange={handleFormChange} name="favClass">
+          {classes.map((c) => <option key={c.id} value={c.id}>{c.name}</option>)}
+        </Form.Select>
+      </Form.Group>
+        <Button variant="primary" onClick={clickSubmit}>
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <React.StrictMode>
+   <Container>
+     <Row xs={1}>
+   <Col>
+     <App />
+   </Col></Row>
+   </Container>
+ </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
